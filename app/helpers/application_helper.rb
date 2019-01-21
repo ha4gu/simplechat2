0b548398c85a71279aa8aa25a51b6f116ccf172d
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-01-21 18:34:34 +0900

    ページごとの可変タイトル設定を実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def build_title(page_title = '')
+    base_title = "SimpleChat ver.2"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " - " + base_title
+    end
+  end
+
 end
